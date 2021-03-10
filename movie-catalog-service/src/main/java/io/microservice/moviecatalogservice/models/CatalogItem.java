f566d0c21bd20b89bbdf9a4b7f6d3de60211fa10
gmaheshwari2006 <32501070+gmaheshwari2006@users.noreply.github.com>
2021-03-10 17:21:11 -0500

    added first service call

---
+++ movie-catalog-service/src/main/java/io/microservice/moviecatalogservice/models/CatalogItem.java
@@ -0,0 +1,80 @@
+package io.microservice.moviecatalogservice.models;
+
+public class CatalogItem {
+
+	private String name;
+	private String desc;
+	private int rating;
+
+	
+	public CatalogItem(String name, String desc, int rating) {
+		super();
+		this.name = name;
+		this.desc = desc;
+		this.rating = rating;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDesc() {
+		return desc;
+	}
+
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+
+	public int getRating() {
+		return rating;
+	}
+
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((desc == null) ? 0 : desc.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + rating;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CatalogItem other = (CatalogItem) obj;
+		if (desc == null) {
+			if (other.desc != null)
+				return false;
+		} else if (!desc.equals(other.desc))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (rating != other.rating)
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "CatalogItem [name=" + name + ", desc=" + desc + ", rating=" + rating + "]";
+	}
+
+}
---
+++ movie-catalog-service/src/main/java/io/microservice/moviecatalogservice/resources/MovieCatalog.java
@@ -0,0 +1,28 @@
+package io.microservice.moviecatalogservice.resources;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.microservice.moviecatalogservice.models.CatalogItem;
+
+@RestController
+@RequestMapping("/catalog")
+public class MovieCatalog {
+
+	@RequestMapping("/{userId}")
+	public List<CatalogItem> getMovieCatalog(@PathVariable("userId") String userId) {
+		/*
+		 * List<CatalogItem> list = new ArrayList<CatalogItem>();
+		 * CatalogItem item = new CatalogItem("gaurav1", "transformer", 5); 
+		 * list.add(item); 
+		 * return list;
+		 */
+		return Collections.singletonList(new CatalogItem("gaurav1", "transformer", 5));
+		
+	}
+}
